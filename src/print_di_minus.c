8eeb8df77bfcbaaf66bfb6832c0e72a92c1bdcc4
Kris Stallenberg <krisstallenberg@gmail.com>
2020-05-11 11:30:49 +0200

    %d test

--- src/print_di_minus.c
+++ src/print_di_minus.c
@@ -6,7 +6,7 @@
 /*   By: kstallen <kstallen@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/02/27 15:57:35 by kstallen      #+#    #+#                 */
-/*   Updated: 2020/04/22 13:37:41 by kris          ########   odam.nl         */
+/*   Updated: 2020/05/11 11:29:58 by kris          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -111,7 +111,8 @@ void	print_di_null_minus(t_data *data)
 		putchar_printf('0', data);
 		field_width--;
 	}
-	print_null_num(data, field_width);
+	putnbr_printf(data->arg.li, data);
+	// print_null_num(data, field_width);
 	while (field_width > 1)
 	{
 		putchar_printf(fill, data);
--- test.c
+++ test.c
@@ -6,9 +6,34 @@ int     main(void)
     int ret1;
     int ret2;
     
-    ret1 = ft_printf("[%p]", NULL);
+    ret1 = ft_printf("[%d]", 0);
     printf("\n");
-    ret2 = printf("[%p]", NULL);
+    ret2 = printf("[%d]", 0);
     printf("\nmy printf: %d\nsy printf:%d", ret1, ret2);
+
+    printf("\n");
+    ret1 = ft_printf("[%-d]", 0);
+    printf("\n");
+    ret2 = printf("[%-d]", 0);
+    printf("\nmy printf: %d\nsy printf:%d", ret1, ret2);
+
+    printf("\n");
+    ret1 = ft_printf("[%-2.9d]", 0);
+    printf("\n");
+    ret2 = printf("[%-2.9d]", 0);
+    printf("\nmy printf: %d\nsy printf:%d", ret1, ret2);
+    
+    printf("\n");
+    ret1 = ft_printf("[%u]", 0);
+    printf("\n");
+    ret2 = printf("[%u]", 0);
+    printf("\nmy printf: %d\nsy printf:%d", ret1, ret2);
+
+    printf("\n");
+    ret1 = ft_printf("[%-u]", 0);
+    printf("\n");
+    ret2 = printf("[%-u]", 0);
+    printf("\nmy printf: %d\nsy printf:%d", ret1, ret2);
+
     return (0);
 }
